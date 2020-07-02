d43234294cb79c5ae5ada84978c0d611cd456925
maeshu-23 <maeshu619@gmail.com>
2020-07-02 15:57:59 +0900

    chat_test

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,8 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
+  
 
   def show_last_message
     if (last_message = messages.last).present?
